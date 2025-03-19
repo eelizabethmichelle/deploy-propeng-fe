8d76ecbe6d662ccde54696db20bc799bfb2dc77a
Muhammad Fawwaz Arshad Said <muhammad.fawwaz22@ui.ac.id>
2025-03-20 04:56:02 +0700

    hsts klo abil hts

--- fe-propeng/next.config.js
+++ fe-propeng/next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // HSTS ke semua halaman
+        headers: [
+          {
+            key: "Strict-Transport-Security", // HSTS
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
