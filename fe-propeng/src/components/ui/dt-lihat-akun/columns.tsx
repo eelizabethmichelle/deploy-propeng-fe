fc17ef307af2ab0f28c9841f984cdc44c22dcd24
eelizabethmichelle <124945174+eelizabethmichelle@users.noreply.github.com>
2025-03-21 02:11:39 +0700

    Update columns.tsx cape

--- fe-propeng/src/components/ui/dt-lihat-akun/columns.tsx
+++ fe-propeng/src/components/ui/dt-lihat-akun/columns.tsx
@@ -86,18 +86,14 @@ export const columns: ColumnDef<Schema>[] = [
     cell: ({ row }) => {
       const isActive = row.getValue("isActive") === true;
       return (
-        <div className="flex w-[100px] items-center">
-          {isActive ? (
-            <>
-              <CheckCircle size={20} className="text-green-600" />
-            </>
+         <div className="flex w-[100px] items-center">
+          {type === "Active" ? (
+            <CheckCircle size={20} className="mr-2 text-green-500" />
           ) : (
-            <>
-              <X size={20} className="text-red-500" />
-            </>
+            <X size={20} className="mr-2 text-red-500" />
           )}
           <span className="capitalize">
-            {isActive ? "Aktif" : "Tidak Aktif"}
+            {type === "Active" ? "Aktif" : "Tidak Aktif"}
           </span>
         </div>
       );
