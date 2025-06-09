9955d9178a92ba9fb5a1d5b1c7a392d118944b32
bangabb <arrayabless@gmail.com>
2025-06-09 14:15:27 +0700

    new backend

--- fe-propeng/src/lib/api.ts
+++ fe-propeng/src/lib/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://outstanding-tierney-arrayaabil-1d6770c2.koyeb.app';
+export const API_BASE_URL = 'https://simakanglo-drffdpgkhyffgyfw.northeurope-01.azurewebsites.net/';
