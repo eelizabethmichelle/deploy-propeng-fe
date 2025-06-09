40e18e8652a298f2037bb011b6a4eb1d19cb29f4
bangabb <arrayabless@gmail.com>
2025-06-09 17:27:00 +0700

    changing to development backend

--- fe-propeng/src/lib/api.ts
+++ fe-propeng/src/lib/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://simakanglo-drffdpgkhyffgyfw.northeurope-01.azurewebsites.net';
+// export const API_BASE_URL = 'https://simakanglo-drffdpgkhyffgyfw.northeurope-01.azurewebsites.net';
 // export const API_BASE_URL = 'http://localhost:8000';
-// export const API_BASE_URL = 'https://outstanding-tierney-arrayaabil-1d6770c2.koyeb.app';
+export const API_BASE_URL = 'https://outstanding-tierney-arrayaabil-1d6770c2.koyeb.app';
